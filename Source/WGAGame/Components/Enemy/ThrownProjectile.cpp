fd6571c27c341a48dfdcd36f503bcd55bdc2e304
untitledexr <jupallll@bk.ru>
2021-05-19 01:28:43 +0300

    wtf

---
+++ Source/WGAGame/Components/Enemy/ThrownProjectile.cpp
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ThrownProjectile.h"
+
+
+// Sets default values
+AThrownProjectile::AThrownProjectile()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	OnActorBeginOverlap.AddDynamic(this, &AThrownProjectile::OnOverlapBegin);
+}
+
+void AThrownProjectile::OnOverlapBegin(AActor* MyOverlappedActor, AActor* OtherActor)
+{
+	if (OtherActor != GetOwner())
+	{
+        Notifier.Broadcast(OtherActor);
+		OnOverlapEnd();
+	}
+}
+
+// Called when the game starts or when spawned
+void AThrownProjectile::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AThrownProjectile::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/WGAGame/Components/Enemy/ThrownProjectile.h
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "GameFramework/Actor.h"
+#include "ThrownProjectile.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FProjectileNotifier, AActor*, OverlappedActor);
+
+UCLASS()
+class WGAGAME_API AThrownProjectile : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AThrownProjectile();
+	
+	UFUNCTION()
+    void OnOverlapBegin(AActor* MyOverlappedActor, AActor* OtherActor);
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnOverlapEnd();
+	
+    virtual void Tick(float DeltaTime) override;
+	
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	
+	FProjectileNotifier Notifier;
+};
