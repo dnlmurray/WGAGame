19aea0c1e6662eb00dbdbfe291a8c95b0d63df8c
valeria <urgbal@gmail.com>
2021-05-27 09:12:46 +0300

    fixed evil force checkpoint save

--- Source/WGAGame/WGAGameGameModeBase.cpp
+++ Source/WGAGame/WGAGameGameModeBase.cpp
@@ -93,7 +93,7 @@ void AWGAGameGameModeBase::LoadCheckpoint()
 	AMainCharacter* MC = StaticCast<AMainCharacter*>(UGameplayStatics::GetPlayerCharacter(GetWorld(), 0));
 	MC->ResetPlayer();
 	MC->SetActorLocation(CheckpointSpawnLocation);
-
+	
 
 }
 
